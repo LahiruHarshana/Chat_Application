4c769b8981f183fa42532aef20c76b3a8ee09773
lahiruharshana <lharshana2002@>
2023-07-03 00:47:00 +0530

    add view

--- src/main/java/lk/ijse/chatApplication/AppInitializer.java
+++ src/main/java/lk/ijse/chatApplication/AppInitializer.java
@@ -1,6 +1,6 @@
-package org.example;
+package lk.ijse.chatApplication;
 
-public class Main {
+public class AppInitializer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/lk/ijse/chatApplication/Launcher.java
@@ -0,0 +1,5 @@
+package lk.ijse.chatApplication;
+
+public class Launcher {
+
+}
