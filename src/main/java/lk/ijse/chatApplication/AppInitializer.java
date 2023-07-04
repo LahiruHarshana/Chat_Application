13c61db6d2c6e0e5ab67c9ebdcc1f1c732e78ecc
lahiruharshana <lharshana2002@>
2023-07-04 18:52:22 +0530

    add home

--- src/main/java/lk/ijse/chatApplication/AppInitializer.java
+++ src/main/java/lk/ijse/chatApplication/AppInitializer.java
@@ -1,7 +1,23 @@
 package lk.ijse.chatApplication;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/home_form.fxml"))));
+        primaryStage.getIcons().add(new Image("assests/icons8-send-message-64.png"));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("join chat");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
