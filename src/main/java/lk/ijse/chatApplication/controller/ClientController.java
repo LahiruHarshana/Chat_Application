b78c6ef1a113ce9f5736134bcfd641ef814a75ff
lahiruharshana <lharshana2002@>
2023-07-04 19:09:55 +0530

    0.0.2

--- src/main/java/lk/ijse/chatApplication/controller/ClientController.java
+++ src/main/java/lk/ijse/chatApplication/controller/ClientController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+
 public class ClientController {
     @FXML
     private Label lblClientName;
@@ -29,4 +34,25 @@ public class ClientController {
     private Label lblimg6;
     @FXML
     private ImageView btnLogOut;
+
+    private List<String> fileList;
+
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+
+    private String clientName;
+
+    private final byte[] emojiByteCode = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8C};
+    private final byte[] emojiByteCode1 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8D};
+    private final byte[] emojiByteCode2 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x8F};
+    private final byte[] emojiByteCode3 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x93};
+    private final byte[] emojiByteCode4 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x98};
+    private final byte[] emojiByteCode5 = new byte[]{(byte)0xF0, (byte)0x9F, (byte)0x98, (byte)0x82};
+
+    private final String img1 = new String(emojiByteCode, Charset.forName("UTF-8"));
+    private final String img2 = new String(emojiByteCode1, Charset.forName("UTF-8"));
+    private final String img3 = new String(emojiByteCode2, Charset.forName("UTF-8"));
+    private final String img4 = new String(emojiByteCode3, Charset.forName("UTF-8"));
+    private final String img5 = new String(emojiByteCode4, Charset.forName("UTF-8"));
+    private final String img6 = new String(emojiByteCode5, Charset.forName("UTF-8"));
 }
