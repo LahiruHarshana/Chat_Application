9a893467f0ab9d12848fb29630ae785723f00572
lahiruharshana <lharshana2002@>
2023-07-04 19:12:59 +0530

    0.0.3

--- src/main/java/lk/ijse/chatApplication/controller/HomeFormController.java
+++ src/main/java/lk/ijse/chatApplication/controller/HomeFormController.java
@@ -1,10 +1,20 @@
 package lk.ijse.chatApplication.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class HomeFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeFormController implements Initializable {
     public TextField txtjon;
     public ImageView btnjon;
     static String name="";
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
