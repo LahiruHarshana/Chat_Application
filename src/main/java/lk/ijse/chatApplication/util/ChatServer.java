01fc7885abddb6ced682eef05c246081945adc81
lahiruharshana <lharshana2002@>
2023-07-04 19:43:14 +0530

    0.3.3

--- src/main/java/lk/ijse/chatApplication/util/ChatServer.java
+++ src/main/java/lk/ijse/chatApplication/util/ChatServer.java
@@ -14,5 +14,45 @@ public class ChatServer {
     static DataOutputStream outputStream = null;
 
     public static void start() {
+        public static void start() {
+            Socket clientSocket;
+            try (ServerSocket serverSocket = new ServerSocket(3031)){
+
+                System.out.println("Server started. Waiting for client connections...");
+
+                while (true) {
+                    clientSocket = serverSocket.accept();
+                    System.out.println("Client connected: " + clientSocket);
+                    Socket finalClientSocket = clientSocket;
+                    Thread clientThread = new Thread(() -> {
+                        try {
+                            DataInputStream  inputStream = new DataInputStream(finalClientSocket.getInputStream());
+                            int port = finalClientSocket.getPort();
+                            socketList.add(finalClientSocket);
+
+                            String incomingMessage = "";
+                            System.out.println(incomingMessage);
+                            while (!(incomingMessage = inputStream.readUTF()).equals("finish")) {
+//                            System.out.println("Received from client " + port + ": " + incomingMessage);
+
+                                for(Socket socket:socketList){
+                                    sentMessage(socket,incomingMessage);
+                                }
+                            }
+                            socketList.remove(finalClientSocket);
+                            finalClientSocket.close();
+                            System.out.println("Client disconnected: " + port);
+                        } catch (IOException e) {
+                            System.out.println("disconnected !");
+                            System.exit(0);
+                        }
+                    });
+
+                    clientThread.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
